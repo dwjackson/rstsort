7ebe1de9eb278642b6fc05be94b320dcf3a89c79
David Jackson <dwjackson@users.noreply.github.com>
2020-08-19 14:45:27 -0400

    Implement the ability to tsort from a file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use rstsort::{DigraphParser, TopologicalSortError};
 use std::io;
+use std::env;
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
-    let mut parser = DigraphParser::new();
+    let args: Vec<String> = env::args().collect();
+
     let stdin = io::stdin();
+    
+    let mut input: Box<dyn io::BufRead> = if args.len() > 1 {
+        let file_name = &args[1];
+        let file = match File::open(file_name) {
+            Ok(file) => file,
+            Err(err) => panic!("Could not open file {}: {}", file_name, err),
+        };
+        Box::new(io::BufReader::new(file))
+    } else {
+        Box::new(stdin.lock())
+    };
+
+    let mut parser = DigraphParser::new();
     let mut line = String::new();
     loop {
-        match stdin.read_line(&mut line) {
+        match input.read_line(&mut line) {
             Ok(nbytes) => {
                 let end_of_file = nbytes == 0;
                 if end_of_file {
